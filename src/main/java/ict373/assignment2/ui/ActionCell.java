6ab54ef2053c02132be0b6c1f5ed3f9f66bf6e76
nwnisworking <nwnisworking@gmail.com>
2026-03-13 08:32:49 +0800

    Create DataTableView that handle button click and row.

---
+++ src/main/java/ict373/assignment2/ui/ActionCell.java
@@ -0,0 +1,63 @@
+package ict373.assignment2.ui;
+
+import ict373.assignment2.events.ButtonEvent;
+import ict373.assignment2.ui.table.DataTableView;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.layout.HBox;
+
+public class ActionCell<T> extends TableCell<T, Void>{
+  private final Button edit_btn = new Button("Edit");
+    
+  private final Button delete_btn = new Button("Delete");
+    
+  private final HBox container = new HBox(10, edit_btn, delete_btn);
+
+  public ActionCell(){
+    container.setAlignment(Pos.CENTER);
+
+    edit_btn.setMinWidth(USE_PREF_SIZE);
+    delete_btn.setMinWidth(USE_PREF_SIZE);
+
+    edit_btn.getStyleClass().addAll("btn", "blue");
+    delete_btn.getStyleClass().addAll("btn", "red");
+    
+    edit_btn.visibleProperty().bind(edit_btn.managedProperty());
+    delete_btn.visibleProperty().bind(delete_btn.managedProperty());
+  }
+  
+  public void displayButton(DataTableView.Button button){
+    edit_btn.setManaged(false);
+    delete_btn.setManaged(false);
+    
+    switch(button){
+      case DataTableView.Button.DELETE -> delete_btn.setManaged(true);
+      case DataTableView.Button.EDIT -> edit_btn.setManaged(true);
+      case DataTableView.Button.ALL -> {
+        edit_btn.setManaged(true);
+        delete_btn.setManaged(true);
+      }
+    }
+  }
+    
+  @Override
+  protected void updateItem(Void item, boolean empty){
+    super.updateItem(item, empty);
+        
+    T data = getTableRow().getItem();
+        
+    if(data != null){
+      edit_btn.setOnAction(e -> {
+        e.consume();
+        fireEvent(new ButtonEvent<>(data, "Edit"));
+      });
+      delete_btn.setOnAction(e -> {
+        e.consume();
+        fireEvent(new ButtonEvent<>(data, "Delete"));
+      });
+    }
+    
+    setGraphic(empty ? null : container);
+  }
+}
---
+++ src/main/java/ict373/assignment2/ui/table/DataTableView.java
@@ -0,0 +1,97 @@
+package ict373.assignment2.ui.table;
+
+import ict373.assignment2.events.ButtonEvent;
+import ict373.assignment2.events.RowEvent;
+import ict373.assignment2.ui.ActionCell;
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+
+public class DataTableView<S> extends TableView<S>{
+  public enum Button{
+    EDIT,
+    DELETE,
+    ALL;
+  };
+  
+  private final TableColumn<S, Void> action_column = new TableColumn<>();
+
+  private final ObjectProperty<EventHandler<ButtonEvent<S>>> on_button_clicked = new SimpleObjectProperty<>();
+
+  private final ObjectProperty<EventHandler<RowEvent<S>>> on_row_clicked = new SimpleObjectProperty<>();
+  
+  private final ObjectProperty<Button> show = new SimpleObjectProperty<>(Button.ALL);
+  
+  public DataTableView(){
+    super();
+    
+    action_column.setText("Action");
+    action_column.setCellFactory(param -> {
+      ActionCell<S> cell = new ActionCell<>();
+      
+      cell.displayButton(show.getValue());
+      
+      return cell;
+    });
+    
+    addEventHandler(ButtonEvent.BUTTON_EVENT, e -> {
+      EventHandler<ButtonEvent<S>> handler = on_button_clicked.getValue();
+      
+      if(handler != null) handler.handle(e);
+    });
+    
+    setRowFactory(param -> {
+      TableRow<S> row = new TableRow<>();
+      
+      row.setOnMouseClicked(e -> {
+        EventHandler<RowEvent<S>> handler = on_row_clicked.getValue();
+        
+        if(handler != null) handler.handle(new RowEvent<>(row.getItem(), "View"));
+      });
+      return row;
+    });
+    Platform.runLater(() -> {
+      getColumns().addLast(action_column);
+      action_column.prefWidthProperty().bind(widthProperty().multiply(.1));
+      action_column.setMinWidth(120);
+    });
+    
+    setColumnResizePolicy(CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+  }
+  
+  public void setOnButtonClicked(EventHandler<ButtonEvent<S>> handler){
+    on_button_clicked.setValue(handler);
+  }
+  
+  public EventHandler<ButtonEvent<S>> getOnButtonClicked(){
+    return on_button_clicked.getValue();
+  }
+  
+  public ObjectProperty<EventHandler<ButtonEvent<S>>> onButtonClickedProperty(){
+    return on_button_clicked;
+  }
+  
+  public void setOnRowClicked(EventHandler<RowEvent<S>> handler){
+    on_row_clicked.setValue(handler);
+  }
+  
+  public EventHandler<RowEvent<S>> getOnRowClicked(){
+    return on_row_clicked.getValue();
+  }
+  
+  public ObjectProperty<EventHandler<RowEvent<S>>> onRowClickedProperty(){
+    return on_row_clicked;
+  }
+
+  public void setShow(Button button){
+    show.setValue(button);
+  }
+  
+  public Button getShow(){
+    return show.getValue();
+  }
+}
