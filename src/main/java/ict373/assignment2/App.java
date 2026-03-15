60d8fdac9339ce3545f9c86ee5da649870e6c52a
nwnisworking <nwnisworking@gmail.com>
2026-03-15 12:20:04 +0800

    Document App.java methods and class with Javadoc comments.

--- src/main/java/ict373/assignment2/App.java
+++ src/main/java/ict373/assignment2/App.java
@@ -1,6 +1,7 @@
 package ict373.assignment2;
 
 import ict373.assignment2.controllers.LoaderController;
+import ict373.assignment2.events.WindowEvent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,25 +13,46 @@ import java.net.URL;
 import javafx.scene.Node;
 import javafx.stage.StageStyle;
 
+/**
+ * <strong>App class</strong>
+ * 
+ * <p>The main entry point for the application. It is responsible for loading the main window and 
+ * providing a method for loading FXML files with their respective controllers or nodes.</p>
+ * 
+ * @author nwnisworking
+ * @date 2/3/2026
+ * @filename App.java
+ */
 public class App extends Application{
-
+  /**
+   * The scene for the application.
+   */
   private static Scene scene;
 
+  /**
+   * Start the application by loading the main window FXML and setting the stage.
+   */
   @Override
   public void start(Stage stage) throws IOException{
-    stage.initStyle(StageStyle.TRANSPARENT);
     scene = new Scene(loadFXML("ui/Window", new LoaderController()), 840, 480);
 
+    stage.initStyle(StageStyle.TRANSPARENT);
     stage.setScene(scene);
     stage.show();
+    stage.fireEvent(new WindowEvent(WindowEvent.OPEN));
   }
 
+  /**
+   * Load an FXML file and set its controller or root node. This is primarily used for <code>&lt;fx:root&gt;</code>.
+   * @param fxml The name of the FXML file to load (without the .fxml extension).
+   * @param node The node to set as the root and controller of the FXML.
+   * @return The loaded parent node from the FXML file, or null if it fails.
+   */
   public static Parent loadFXML(String fxml, Node node){
     try{
       URL url = App.class.getResource(fxml + ".fxml");
       FXMLLoader fxmlLoader = new FXMLLoader(url);
 
-      // This is used for custom elements
       if(node != null){
         fxmlLoader.setRoot(node);
         fxmlLoader.setController(node);
@@ -41,13 +63,18 @@ public class App extends Application{
       return fxmlLoader.load();
     }
     catch(IOException ex){
-      ex.printStackTrace();
       System.out.println("[App]: Unable to load FXML for Node");
     }
     
     return null;
   }
   
+  /**
+   * Load an FXML file and set its controller.
+   * @param fxml The name of the FXML file to load (without the .fxml extension).
+   * @param controller The controller to set for the FXML.
+   * @return The loaded parent node from the FXML file, or null if it fails.
+   */
   public static Parent loadFXML(String fxml, Object controller){
     try{
       URL url = App.class.getResource(fxml + ".fxml");
@@ -66,7 +93,11 @@ public class App extends Application{
     
     return null;
   }
-    
+ 
+  /**
+   * The main method to launch the application.
+   * @param args
+   */
   public static void main(String[] args){
     launch();
   }
