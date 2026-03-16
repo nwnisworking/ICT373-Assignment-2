aeebb2a8909854890c035993640107cb6532f8c5
nwnisworking <nwnisworking@gmail.com>
2026-03-16 22:23:09 +0800

    Refactor DataTableView to remove unused event handlers and simplify row click handling for improved clarity. The event will bubble to the parent until it is consumed.
    [SQUASH] Squash with previous DataTableView update.

--- src/main/java/ict373/assignment2/ui/table/DataTableView.java
+++ src/main/java/ict373/assignment2/ui/table/DataTableView.java
@@ -1,12 +1,10 @@
 package ict373.assignment2.ui.table;
 
-import ict373.assignment2.events.ButtonEvent;
-import ict373.assignment2.events.RowEvent;
+import ict373.assignment2.events.TableEvent;
 import ict373.assignment2.ui.ActionCell;
 import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.event.EventHandler;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -21,10 +19,6 @@ public class DataTableView<S> extends TableView<S>{
   
   private final TableColumn<S, Void> action_column = new TableColumn<>();
 
-  private final ObjectProperty<EventHandler<ButtonEvent<S>>> on_button_clicked = new SimpleObjectProperty<>();
-
-  private final ObjectProperty<EventHandler<RowEvent<S>>> on_row_clicked = new SimpleObjectProperty<>();
-  
   private final ObjectProperty<Button> show = new SimpleObjectProperty<>(Button.ALL);
   
   public DataTableView(){
@@ -39,55 +33,23 @@ public class DataTableView<S> extends TableView<S>{
       return cell;
     });
 
-    addEventHandler(ButtonEvent.BUTTON_EVENT, e -> {
-      EventHandler<ButtonEvent<S>> handler = on_button_clicked.getValue();
-      
-      if(handler != null) handler.handle(e);
-    });
-    
     setRowFactory(param -> {
       TableRow<S> row = new TableRow<>();
       
       row.setOnMouseClicked(e -> {
-        EventHandler<RowEvent<S>> handler = on_row_clicked.getValue();
-        
-        if(handler != null) handler.handle(new RowEvent<>(row.getItem(), "View"));
+        if(!row.isEmpty()){
+          fireEvent(new TableEvent<>(TableEvent.VIEW, row.getItem()));
+        }
       });
+
       return row;
     });
-    Platform.runLater(() -> {
-      getColumns().addLast(action_column);
-      action_column.prefWidthProperty().bind(widthProperty().multiply(.1));
-      action_column.setMinWidth(120);
-    });
-    
+
+    Platform.runLater(() -> getColumns().addLast(action_column));
+
     setColumnResizePolicy(CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
   }
   
-  public void setOnButtonClicked(EventHandler<ButtonEvent<S>> handler){
-    on_button_clicked.setValue(handler);
-  }
-  
-  public EventHandler<ButtonEvent<S>> getOnButtonClicked(){
-    return on_button_clicked.getValue();
-  }
-  
-  public ObjectProperty<EventHandler<ButtonEvent<S>>> onButtonClickedProperty(){
-    return on_button_clicked;
-  }
-  
-  public void setOnRowClicked(EventHandler<RowEvent<S>> handler){
-    on_row_clicked.setValue(handler);
-  }
-  
-  public EventHandler<RowEvent<S>> getOnRowClicked(){
-    return on_row_clicked.getValue();
-  }
-  
-  public ObjectProperty<EventHandler<RowEvent<S>>> onRowClickedProperty(){
-    return on_row_clicked;
-  }
-
   public void setShow(Button button){
     show.setValue(button);
   }
