256da98c285c0a235d8b40dbeea806069386cca4
nwnisworking <nwnisworking@gmail.com>
2026-03-16 22:08:42 +0800

    Include validate method for validating input fields.

--- src/main/java/ict373/assignment2/FormRecord.java
+++ src/main/java/ict373/assignment2/FormRecord.java
@@ -1,6 +1,8 @@
 package ict373.assignment2;
 
 import ict373.assignment2.ui.inputs.InputField;
+import ict373.assignment2.utils.Validator.ValidatorResult;
+
 import java.util.List;
 
 public interface FormRecord<T>{
@@ -9,4 +11,6 @@ public interface FormRecord<T>{
   void save(T data);
   
   void load(T data);
+
+  ValidatorResult validate();
 }
