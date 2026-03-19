dc53bfa58ba2ae40e099359dff6edbbc60af8bbd
nwnisworking <nwnisworking@gmail.com>
2026-03-19 13:46:16 +0800

    Add HomeController with modal included to Home.fxml.

--- src/main/java/ict373/assignment2/controllers/HomeController.java
+++ src/main/java/ict373/assignment2/controllers/HomeController.java
@@ -1,15 +1,17 @@
 package ict373.assignment2.controllers;
 
 import ict373.assignment2.App;
+import ict373.assignment2.events.ModalEvent;
+import ict373.assignment2.ui.Modal;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.Observable;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 /**
  * <strong>HomeController class</strong>
@@ -25,17 +27,32 @@ public class HomeController implements Initializable{
 	@FXML
 	private ListView<String> nav_items;
 
+  @FXML
+  private StackPane main;
+  
   /** The content area to display different pages */
 	@FXML
 	private BorderPane content;
   
   /**
+   * The modal component used for displaying additional content.
+   */
+  @FXML
+  private Modal modal;
+  
+  /**
    * Initialize the controller by setting up the navigation listener and loading the first page.
+   * 
+   * @param url The location for the root object.
+   * @param rb The resources used to localize the root object.
    */
 	@Override
 	public void initialize(URL url, ResourceBundle rb){
     nav_items.getSelectionModel().selectedItemProperty().addListener(this::loadPage);
 		nav_items.getSelectionModel().selectFirst();
+    
+    content.addEventHandler(ModalEvent.OPEN, this::openModal);
+    content.addEventHandler(ModalEvent.CLOSE, this::closeModal);
 	}
   
   /**
@@ -60,5 +77,25 @@ public class HomeController implements Initializable{
       content.setCenter(node);
     
     System.out.println("[Page]: " + new_value + " page loaded.");
+  }
+  
+  /**
+   * Event handler to open the modal with the provided content node.
+   * 
+   * @param event The ModalEvent containing the node to be displayed in the modal. 
+   */
+  private void openModal(ModalEvent event){
+    event.consume();
+    modal.open(event.getNode());
+  }
+  
+  /**
+   * Event handler to close the modal when a close event is triggered.
+   * 
+   * @param event The ModalEvent triggered to close the modal.
+   */
+  private void closeModal(ModalEvent event){
+    event.consume();
+    modal.close();
   }
 }
