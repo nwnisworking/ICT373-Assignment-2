bdcfc310316cc97aa86b7dcee5a478fcaa6e58cf
nwnisworking <nwnisworking@gmail.com>
2026-03-21 16:25:54 +0800

    Added toast notification to enhance to deliver status and message to user.

---
+++ src/main/java/ict373/assignment2/events/ToastEvent.java
@@ -0,0 +1,33 @@
+package ict373.assignment2.events;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+
+public class ToastEvent extends Event{
+  public enum Status{
+    SUCCESS,
+    WARN,
+    ERROR,
+    INFO;
+  };
+  
+  public static final EventType<ToastEvent> ANY = new EventType<>(Event.ANY, "TOAST_EVENT");
+  
+  private final Status status;
+  
+  private final String message;
+  
+  public ToastEvent(EventType<ToastEvent> type, Status status, String message){
+    super(type);
+    this.status = status;
+    this.message = message;
+  }
+  
+  public Status getStatus(){
+    return status;
+  }
+  
+  public String getMessage(){
+    return message;
+  }
+}
