358e836dacf420c28c1de753d21e63927185dddb
nwnisworking <nwnisworking@gmail.com>
2026-03-26 13:55:51 +0800

    Update view when customer is edited.

--- src/main/java/ict373/assignment2/controllers/customer/CustomerController.java
+++ src/main/java/ict373/assignment2/controllers/customer/CustomerController.java
@@ -186,10 +186,10 @@ public class CustomerController implements Initializable{
       subscription_service.remove(customer);  
       content.fireEvent(new ToastEvent(ToastEvent.ANY, ToastEvent.Status.SUCCESS, customer + " deleted"));
       updatePageCount();
-      updateTableView(pagination.getCurrentPageIndex());
     }
     else{
       content.fireEvent(new ToastEvent(ToastEvent.ANY, ToastEvent.Status.SUCCESS, customer + " data modified"));
+      updateTableView(pagination.getCurrentPageIndex());
     }
   }
   
